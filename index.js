a1b5fcf2bd864b968db1e0b94458f89799efe2ff
jerry-kjk <jerrykajik@gmail.com>
2022-11-28 22:42:23 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+   // Write your code here
+//multiplication
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+//Generate a random integer use Math.random()
+//number between 0 - 0.999
+// Multiply by 10 to give us a number between 0 and 9.999
+
+
+let random = Number.isInteger(Math.random() * 10) + 1;
+
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3%num4;
+
+
+let max = Math.max(12,6,7,4,15,17,19,20)
+max;
